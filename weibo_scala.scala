fe134fa9c4974d7c018690d31e246d5a61dc2440
logep <pingbeer69@gmail.com>
2023-09-19 09:19:21 +0800

    Update weibo_scala.scala

--- weibo_scala.scala
+++ weibo_scala.scala
@@ -1,35 +1,48 @@
 import java.io.{BufferedWriter, File, FileWriter}
+import java.net.{HttpURLConnection, URL}
+import java.nio.file.{Files, Paths}
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-
-import scala.io.Source
 import scala.util.{Failure, Success, Try}
-import sttp.client3._
-import io.circe.parser._
+import io.circe.Json
+import io.circe.parser.parse
+import io.circe._
+import io.circe.generic.semiauto._
 
 object WeiboHotTopics {
+  case class HotTopic(word: String)
+  case class HotTopicsResponse(data: Data)
+  case class Data(realtime: List[HotTopic])
+  implicit val hotTopicDecoder: Decoder[HotTopic] = deriveDecoder[HotTopic]
   def main(args: Array[String]): Unit = {
     val hotTopics = getHotTopics()
-    hotTopics.foreach { topics =>
-      addHotTopicsToRepository(topics)
+    hotTopics match {
+      case Success(topics) =>
+        addHotTopicsToRepository(topics) match {
+          case Success(_) => println("Hot topics added to repository successfully!")
+          case Failure(e) => println(s"Failed to add hot topics to repository: $e")
+        }
+      case Failure(e) => println(s"Failed to get hot topics: $e")
     }
   }
 
-  def getHotTopics(): Option[List[HotTopic]] = {
-    val response = basicRequest.get(uri"https://weibo.com/ajax/side/hotSearch").send()
-    response.body match {
-      case Right(jsonStr) =>
-        val parsedJson = parse(jsonStr)
-        parsedJson match {
-          case Right(json) =>
-            val data = json.hcursor.downField("data").downField("realtime").as[List[HotTopic]]
-            data match {
-              case Right(topics) => Some(topics)
-              case Left(_)       => None
-            }
-          case Left(_) => None
-        }
-      case Left(_) => None
+  def getHotTopics(): Try[List[HotTopic]] = {
+    Try {
+      val url = new URL("https://weibo.com/ajax/side/hotSearch")
+      val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+      connection.setRequestMethod("GET")
+      connection.connect()
+
+      val inputStream = connection.getInputStream
+      val responseBody = scala.io.Source.fromInputStream(inputStream).mkString
+
+      val parsedJson = parse(responseBody)
+      parsedJson match {
+        case Right(json) =>
+          val data = json.hcursor.downField("data").downField("realtime").as[List[HotTopic]](Decoder.decodeList(hotTopicDecoder))
+          data.getOrElse(List.empty)
+        case Left(_) => List.empty
+      }
     }
   }
 
@@ -43,28 +56,27 @@ object WeiboHotTopics {
     header + topicList
   }
 
-  def addHotTopicsToRepository(hotTopics: List[HotTopic]): Unit = {
-    val currentDateTime = LocalDateTime.now()
-    val dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-    val timeFormatter = DateTimeFormatter.ofPattern("HHmmss")
+  def addHotTopicsToRepository(hotTopics: List[HotTopic]): Try[Unit] = {
+    Try {
+      val currentDateTime = LocalDateTime.now()
+      val dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+      val timeFormatter = DateTimeFormatter.ofPattern("HHmmss")
 
-    val dateStr = currentDateTime.format(dateFormatter)
-    val folderName = s"weibo/$dateStr"
-    val folder = new File(folderName)
-    if (!folder.exists()) {
-      folder.mkdirs()
+      val dateStr = currentDateTime.format(dateFormatter)
+      val folderName = s"weibo/$dateStr"
+      val folderPath = Paths.get(folderName)
+
+      if (!Files.exists(folderPath)) {
+        Files.createDirectories(folderPath)
+      }
+
+      val fileName = s"hot_topics_${currentDateTime.format(timeFormatter)}.md"
+      val filePath = Paths.get(folderName, fileName)
+      val markdownContent = generateMarkdownContent(hotTopics)
+
+      val writer = new BufferedWriter(new FileWriter(filePath.toString))
+      writer.write(markdownContent)
+      writer.close()
     }
-
-    val fileName = s"hot_topics_${currentDateTime.format(timeFormatter)}.md"
-    val filePath = s"$folderName/$fileName"
-    val markdownContent = generateMarkdownContent(hotTopics)
-
-    val file = new File(filePath)
-    val writer = new BufferedWriter(new FileWriter(file))
-    writer.write(markdownContent)
-    writer.close()
   }
 }
-
-case class HotTopic(word: String)
-
